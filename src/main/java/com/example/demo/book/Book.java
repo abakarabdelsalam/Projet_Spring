f57370416550408644991eb6f93bcb0f3095342b
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-09-23 10:57:26 +0200

    the Entity

--- src/main/java/com/example/demo/book/Book.java
+++ src/main/java/com/example/demo/book/Book.java
@@ -1,15 +1,64 @@
 package com.example.demo.book;
 
 
+import com.example.demo.User.User;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @NotBlank
     private  String title;
-    private  Category category;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private User user;
+
+    @Transient
+    private int categoryId;
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 
     private BookStatus bookStatus;
+    private boolean deleted;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
 
     public BookStatus getBookStatus() {
         return bookStatus;
