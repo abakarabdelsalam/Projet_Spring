2b4be438a390d574ff60aa8ec3034a30bab0ba55
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-09-29 16:47:58 +0200

    partie inscription

--- src/main/java/com/example/demo/User/UserController.java
+++ src/main/java/com/example/demo/User/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.User;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,14 +10,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @PostMapping(value = "/users")
     public ResponseEntity addUSer(@Valid @RequestParam User user){
-      User userOld = new User("toto@gmail.com");
-        return new ResponseEntity( userOld, HttpStatus.CREATED);
+
+        List<User> users = userRepository.findByEmailId(user.getEmail());
+
+        if (!users.isEmpty()){
+            return new ResponseEntity("User nexiste pas", HttpStatus.BAD_REQUEST);
+
+        }
+
+        userRepository.save(user);
+        return new ResponseEntity( user, HttpStatus.CREATED);
 
     }
 
